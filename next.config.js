5a5344f5f52b5e2885f5f8f67b68cfa0358e00ed
superadmin <aida_bot@outlook.com>
2025-05-23 20:54:00 +0800

    1

--- next.config.js
+++ next.config.js
@@ -1,33 +1,7 @@
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    webpack: (config, { dev, isServer }) => {
-        // 优化 CSS 处理
-        if (!dev && !isServer) {
-            config.optimization.minimizer.push(
-                new CssMinimizerPlugin({
-                    minimizerOptions: {
-                        preset: [
-                            'default',
-                            {
-                                discardComments: { removeAll: true },
-                            },
-                        ],
-                    },
-                })
-            );
-        }
-        return config;
-    },
-    experimental: {
-        css: {
-            // 禁用 lightningcss，回退到 postcss
-            implementation: require('postcss'),
-        },
-    },
 };
 
 module.exports = nextConfig;
